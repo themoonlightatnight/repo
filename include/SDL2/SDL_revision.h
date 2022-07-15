1605e702aa33745a4d2f74ee05cbc7753109ff00
taitai94 <taiapple22@gmail.com>
2022-07-15 16:30:28 +0700

    sdl glew lib added susscessfully

---
+++ include/SDL2/SDL_revision.h
@@ -0,0 +1,2 @@
+#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@37aecda2c1377d923a6dca8a161a7d31f2c327e4"
+#define SDL_REVISION_NUMBER 0
--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-#include "camera.h"
+#include "SDL2/SDL.h"
+#include "GL/gl.h"
 int main()
 {
-	Camera2D camera;
-	camera.init(5,18);
-	camera.printPos();
-	return 0;
+	/*SDL_Window* m_sdlWindow;
+	float screenWidth = 200;
+	float screenHeight = 200;
+SDL_GLContext m_glContext;
+Uint32 m_flag = SDL_WINDOW_OPENGL;
+	m_sdlWindow = SDL_CreateWindow("Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, m_flag);
+glewInit();*/
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("error initializing SDL: %s\n", SDL_GetError());
+    }
+    SDL_Window* win = SDL_CreateWindow("Engine",
+                                       SDL_WINDOWPOS_CENTERED,
+                                       SDL_WINDOWPOS_CENTERED,
+                                       300, 300, 0);
+    while (1)
+        ;
+return 0;
 }
